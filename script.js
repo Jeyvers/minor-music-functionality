3f4db01d82f64d01d12c54435b1c2e6ddacaf0a6
Jeyi Adole <erthnice@gmail.com>
2021-09-11 18:14:50 +0100

    Edited(Better UI)

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 const theSearchSong = document.getElementById("theSearchSong");
 const theSearchIcon = document.getElementById("theSearchIcon");
-const sinnerSong = document.getElementById("sinnerSong");
-const sinnerIcon = document.getElementById("sinnerIcon");
+const olderSong = document.getElementById("olderSong");
+const olderIcon = document.getElementById("olderIcon");
+const promisesSong = document.getElementById("promisesSong");
+const promisesIcon = document.getElementById("promisesIcon");
 const tennesseSong = document.getElementById("tennesseSong");
 const tennesseIcon = document.getElementById("tennesseIcon");
+const billieSong = document.getElementById("billieSong");
+const billieIcon = document.getElementById("billieIcon");
 const cityLightsSong = document.getElementById("cityLightsSong");
 const cityLightsIcon = document.getElementById("cityLightsIcon");
 
 const searchPause = document.querySelector("#searchPause");
-const sinnerPause = document.querySelector("#sinnerPause");
+const olderPause = document.querySelector("#olderPause");
+const promisesPause = document.querySelector("#promisesPause");
 const tennessePause = document.querySelector("#tennessePause");
+const billiePause = document.querySelector("#billiePause");
 const cityLightsPause = document.querySelector("#cityLightsPause");
 
 // nfIcon.onclick =  () => nfSong.play();
@@ -34,24 +40,45 @@ searchPause.onclick = () => {
     searchPause.classList.remove('active')
 }
 
-// ADEKUNLE GOLD
-sinnerIcon.onclick =  () => {
-   if(sinnerSong.paused){
-        sinnerSong.play();
-        sinnerIcon.classList.add('hidden');
-        sinnerPause.classList.add('active');
+
+// SASHA OLDER
+olderIcon.onclick =  () => {
+   if(olderSong.paused){
+        olderSong.play();
+        olderIcon.classList.add('hidden');
+        olderPause.classList.add('active');
         
    }
     else {
-    sinnerSong.pause();
+    olderSong.pause();
    
    }
 }
 
-sinnerPause.onclick = () => {
-    sinnerSong.pause();
-    sinnerIcon.classList.remove('hidden')
-    sinnerPause.classList.remove('active')
+olderPause.onclick = () => {
+    olderSong.pause();
+    olderIcon.classList.remove('hidden')
+    olderPause.classList.remove('active')
+}
+
+// SASHA OLDER
+promisesIcon.onclick =  () => {
+   if(promisesSong.paused){
+        promisesSong.play();
+        promisesIcon.classList.add('hidden');
+        promisesPause.classList.add('active');
+        
+   }
+    else {
+    promisesSong.pause();
+   
+   }
+}
+
+promisesPause.onclick = () => {
+    promisesSong.pause();
+    promisesIcon.classList.remove('hidden')
+    promisesPause.classList.remove('active')
 }
 
 
@@ -76,10 +103,30 @@ tennessePause.onclick = () => {
     tennessePause.classList.remove('active')
 }
 
-
-
-
 // KIIARA TENNESSEE
+billieIcon.onclick =  () => {
+   if(billieSong.paused){
+        billieSong.play();
+        billieIcon.classList.add('hidden');
+        billiePause.classList.add('active');
+        
+   }
+    else {
+    billieSong.pause();
+   
+   }
+}
+
+billiePause.onclick = () => {
+    billieSong.pause();
+    billieIcon.classList.remove('hidden')
+    billiePause.classList.remove('active')
+}
+
+
+
+
+// CITY LIGHTS
 cityLightsIcon.onclick =  () => {
    if(cityLightsSong.paused){
         cityLightsSong.play();
