6561c7154fc15f30383b862678d34bf90c492d7d
Jeyi Adole <erthnice@gmail.com>
2021-09-23 16:06:50 +0100

    Tried

--- trial3/script.js
+++ trial3/script.js
@@ -1,13 +1,14 @@
 let showcaseContainer = document.getElementById('showcase-container');
 let audioList = document.getElementById('audio-list');
-document.addEventListener('DOMContentLoaded', loadMusicList)
-document.addEventListener('click', playMusic)
+document.addEventListener('DOMContentLoaded', loadMusicList);
+document.querySelector('body').addEventListener('click', playMusic);
 
 
 function loadMusicList(){
     fetch('music.json')
     .then((res) => res.json())
     .then((data) => {
+        
         let output = '';
 
         data.forEach(function(music){
@@ -36,36 +37,64 @@ function loadMusicList(){
     </div>
    
     <div class="buttons">
-    <button class="btn playIcon" id="playIcon">
+    <button class="btn playIcon" id="playIcon${music.id}">
     <i class="uil uil-play-circle icon"></i> Play
-</button>
-<button  class="btn hidden pauseIcon" id="pauseIcon">
-    <i class="uil uil-pause-circle"></i> Pause
-</button>
+    </button>
+    <button  class="btn hidden pauseIcon" id="pauseIcon${music.id}">
+        <i class="uil uil-pause-circle"></i> Pause
+    </button>
 
-<button class="btn">
-    <a download="" href="${music.music_src}">  <i class="uil uil-download-alt"></i> Download </a>  
-</button>
-    </div>
-    </div>
-        `
+    <button class="btn">
+        <a download="" href="${music.music_src}">  <i class="uil uil-download-alt"></i> Download </a>  
+    </button>
+        </div>
+
+    <audio id="theSong${music.id}">
+    <source src="${music.music_src}" type="audio/mp3">
+    </audio>
+    </div>   
+        `        
+        
     });
 
     showcaseContainer.innerHTML = output;
-
-    let audio = '';
-    audio += ` <audio id="theSong">
-    <source src="${data.music_src}" type="audio/mp3">
-    </audio>`
-    audioList.innerHTML = audio;
-    console.log(audioList);
+    
     })
-
+   
+    
 }
 
+// Fifth trial of music play functionality
+// Added ${music.id} to above code. 
+// Added the return data - didn't work.
+// Achieved so far : Less HTML code. 
+// Previous trials = created seperate audio element, added it to the audioList in HTML document. 
+
 function playMusic(e){
-    
-    if(e.target.classList.contains('playIcon')){
-    console.log(data)
-    }
+
+        fetch('music.json')
+        .then((res) => res.json())
+        .then((data) => {
+            data.forEach(function(music){
+                if(e.target.classList.contains(`playIcon${music.id}`)) {
+                    let theSong = document.getElementById(`theSong${music.id}`);
+                    let playIcon = document.getElementById(`playIcon${music.id}`);
+                    let pauseIcon = document.getElementById(`pauseIcon${music.id}`);
+       
+                    if(theSong.paused){
+                        theSong.play();
+                        playIcon.classList.add('hidden');
+                        pauseIcon.classList.add('active');
+                        } 
+            
+                    pauseIcon.onclick = () => {
+                        theSong.pause();
+                        playIcon.classList.remove('hidden')
+                        pauseIcon.classList.remove('active')
+                        }  
+                }
+            })
+        })
+        .catch( (err) => console.log(err))
+       
 }
